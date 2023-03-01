ca85d131d070bdc2ea58f6629366eb30be7db85a
Austin Lamp <austinlamp4@gmail.com>
2023-03-01 08:50:17 -0500

    Added additional design to the views being committed, including: - An edit to listings to fix the stacking of individual listings inside of each other (due to forgetting to close a <div>). - An edit to listings to display a default image in the event no image was given with a listing. - An edit to layout to add a footer and header and create a pretty design for the page.

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -7,7 +7,7 @@
 @foreach($listings as $listing)
     <div class="bg-gray-50 border border-gray-200 rounded p-6">
         <div class="flex">
-            <img class="hidden w-48 mr-6 md:block" src="images/acme.png" alt="Company Logo"/>
+            <img class="hidden w-48 mr-6 md:block" src="images/no-image.png" alt="Company Logo"/>
             <div>
                 <h3 class="text-2xl">
                     <a href="show.html">{{$listing->title}}</a>
@@ -32,7 +32,8 @@
                     {{$listing->location}}
                 </div>
             </div>
-        </div>               
+        </div>
+    </div>        
 @endforeach
 @else
     <p>No listings were identified.</p>
